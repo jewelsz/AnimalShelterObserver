418b20a66546c9ccecffb91a67b30f32ef4b875d
jewelsz <jowelle_klomp@hotmail.com>
2018-10-02 11:39:17 +0200

    test aangepast
    Opgesplitst in twee testen voor duidelijkheid

--- Tests/AnimalTest.java
+++ Tests/AnimalTest.java
@@ -1,20 +1,33 @@
 import Animals.Cat;
 import Animals.Gender;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest
 {
+
     @Test
     public void nameTest()
     {
+
         String firstName = "Ozzy";
         String changedName = "Bozzy";
 
         Cat cat1 = new Cat(firstName, Gender.Male, "Krabt aan het behang");
 
         assertEquals(firstName, cat1.getName());
+    }
+
+    @Test
+    public void changeNameTest()
+    {
+
+        String firstName = "Ozzy";
+        String changedName = "Bozzy";
+
+        Cat cat1 = new Cat(firstName, Gender.Male, "Krabt aan het behang");
 
         cat1.setName(changedName);
 
